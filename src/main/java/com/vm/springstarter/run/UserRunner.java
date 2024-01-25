aad0b655c0ebd88107eb7565a24b8547a775ad44
Viktor_Marchuk <viktor.m77.77@mail.ru>
2024-01-25 08:34:01 +0300

    init1

--- src/main/java/com/vm/springstarter/run/UserRunner.java
+++ src/main/java/com/vm/springstarter/run/UserRunner.java
@@ -1,18 +1,12 @@
 package com.vm.springstarter.run;
 
-import com.vm.springstarter.database.repo.UserRepo;
-import com.vm.springstarter.dto.UserDto;
-import com.vm.springstarter.ioc.Container;
-import com.vm.springstarter.mapper.UserMapper;
-import com.vm.springstarter.service.UserService;
-
 public class UserRunner {
     public static void main(String[] args) {
 //        UserRepo userRepo = new UserRepo();
 //        UserDto userDto = new UserDto();
 //        UserMapper userMapper = new UserMapper(userDto);
-        Container container = new Container();
-        UserService userService = container.get(UserService.class);
+//        Container container = new Container();
+//        UserService userService = container.get(UserService.class);
 
     }
 }
