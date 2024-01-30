309ba7a4392280038edd28860852c19310b0687e
Viktor_Marchuk <viktor.m77.77@mail.ru>
2024-01-30 12:25:33 +0300

    learn properties

--- src/main/java/com/vm/springstarter/ApplicationRunner.java
+++ src/main/java/com/vm/springstarter/ApplicationRunner.java
@@ -1,18 +1,22 @@
-package com.vm.springstarter.run;
+package com.vm.springstarter;
 
 
+import com.vm.springstarter.config.DataBaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.core.SpringProperties;
 
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(SpringProperties.getProperty("test.msg"));
-        System.out.println(context.getBean("connect"));
+        System.out.println(context.getBean("connectionPool"));
+        System.out.println(context.getBean(DataBaseProperties.class));
 
     }
 
