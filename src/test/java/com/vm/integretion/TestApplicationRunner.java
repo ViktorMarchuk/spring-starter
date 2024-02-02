f47e87e1498644a0da53af978584749cd4a88c2e
Viktor_Marchuk <viktor.m77.77@mail.ru>
2024-02-02 16:27:17 +0300

    work02

--- src/test/java/com/vm/integretion/TestApplicationRunner.java
+++ src/test/java/com/vm/integretion/TestApplicationRunner.java
@@ -1,6 +1,8 @@
 package com.vm.integretion;
 
 import com.vm.springstarter.pool.ConnectionPool;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
@@ -8,4 +10,6 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 public class TestApplicationRunner {
     @SpyBean(name = "connectionPool")
     private ConnectionPool connectionPool;
+    @PersistenceContext
+    private EntityManager entityManager;
 }
--- src/test/java/com/vm/integretion/repo/CompanyRepositoryTest.java
+++ src/test/java/com/vm/integretion/repo/CompanyRepositoryTest.java
@@ -6,22 +6,47 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @IT
 @RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@TestPropertySource(locations = "classpath:spring.properties")
+@Transactional
 public class CompanyRepositoryTest {
 
-    @Autowired
-    private EntityManager entityManager;
+
+    private final EntityManager entityManager;
 
     @Test
     void findById() {
         Company company = entityManager.find(Company.class, 1);
         assertNotNull(company);
         assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+                .name("Apple")
+                .locales(Map.of("ru", "Apple описание",
+                        "en", "Apple description"))
+                .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+    @Test
+    void test(){
+
     }
 }
